36820b8d5e5b1ef31a62fb3effbaa5e0e43f5120
beka00 <bekob550@gmail.com>
2022-03-29 21:26:40 +0600

    home work

---
+++ bin/fib.dart
@@ -0,0 +1,44 @@
+void main(List<String> arguments) {
+  //1)
+  // List abo = [];
+  // for (int i = 1; i <= 5; i++) {
+  //   abo.add(i);
+  // }
+  // print(abo);
+
+  //2)
+  // List a = [];
+  // for (int i = 5; i > 0; i--) {
+  //   a.add(i);
+  // }
+  // print(a);
+
+  //3)
+  // int a = 3;
+  // for (int i = 1; i <= 10; i++) {
+  //   print('3*$i=${a * i}');
+  // }
+
+  //4)
+  // List a = [];
+  // num res = 0;
+  // for (int i = 100; i <= 200; i++) {
+  //   a.add(i);
+  //   if (i % 17 == 0) {
+  //     (res += i);
+  //   }
+  // }
+  // print(res);
+
+  //5)
+  // int i = 0;
+  // while (i < 98) {
+  //   print(i += 7);
+  // }
+
+  //6)
+  // int i = 1;
+  // while (i < 512) {
+  //   print(i += i);
+  // }
+}
